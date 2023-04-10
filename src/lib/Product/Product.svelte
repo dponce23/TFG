474156227141222301f4f91bf11d19701e17a297
Diego Ponce <diego.ponce@decathlon.com>
2023-04-10 17:32:24 +0200

    feat: mvp pdp

--- src/lib/Product/Product.svelte
+++ src/lib/Product/Product.svelte
@@ -1,12 +1,32 @@
 <script>
-    export let producto = {};
+    import { obtainProducts } from "../Api/apiCall";
+    const initialPos = window.location.href.indexOf("=", 0) + 1;
+    let prodId = window.location.href.substring(
+        initialPos,
+        window.location.href.length
+    );
+    let productInfo = {};
+    let loading = true;
+    try {
+        obtainProducts().then((prods) => {
+            productInfo = prods.find((p) => p.id == prodId);
+            loading = false;
+        });
+    } catch (e) {
+        console.log(e);
+    }
 </script>
 
 <div>
-    <div>
-        <h1>nombre del producto</h1>
-        <img src="../../assets/img_productos/camiseta_blanca.png" alt="" />
-    </div>
+    {#if loading}
+        <div>Loading...</div>
+    {:else}
+        <div class="prod_container">
+            <p class="prod_title">{productInfo.title}</p>
+            <img src={productInfo.image} alt="" />
+            <p class="prod_description">{productInfo.description}</p>
+        </div>
+    {/if}
 </div>
 
 <style>
