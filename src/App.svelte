01ef435871a8feeb9366ac3bc979d00741b51b9e
dponce <diego.ponce@decathlon.com>
2023-05-23 10:46:01 +0200

    Fix(): mock name

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,7 @@
   import Router from "svelte-spa-router";
   import routes from "./lib/routes";
   import Nav from "./lib/Nav/Nav.svelte";
-  import { links } from "./lib/data-mock.json";
+  import { links } from "./lib/buttonLinks_Mock.json";
   import Footer from "./lib/Footer/Footer.svelte";
 </script>
 
