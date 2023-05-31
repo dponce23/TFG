6c84929adfcffeb8021395605a3ad8663ae661cf
dponce <diego.ponce@decathlon.com>
2023-05-31 18:57:07 +0200

    Feat(): added cart icon

--- src/App.svelte
+++ src/App.svelte
@@ -2,14 +2,29 @@
   import Router from "svelte-spa-router";
   import routes from "./lib/routes";
   import Nav from "./lib/Nav/Nav.svelte";
-  import { links } from "./lib/buttonLinks_Mock.json";
+  import { links } from "./lib/buttonLinks_Mock";
   import Footer from "./lib/Footer/Footer.svelte";
+  import { getCookie } from "./lib/Cookies.utils";
+
+  let visible = true;
+  let isLogged = getCookie("UserID") ? true : false;
+
+  if (!isLogged) {
+    visible = false;
+  } else {
+    visible = true;
+  }
 </script>
 
 <main>
   <div>
     <Nav {links} />
   </div>
+  {#if visible}
+    <a class="cart_icon" href="#/carrito"
+      ><img src="src/assets/carrito-de-compras.png" alt="carrito" /></a
+    >
+  {/if}
   <div class="bodyPages">
     <Router {routes} />
   </div>
